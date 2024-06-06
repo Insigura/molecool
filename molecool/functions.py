7434c591f064f8b5cd9f8b22a514a31ce6d3e4ff
Pascal Roney <woahh345@gmail.com>
2024-06-05 17:35:24 -0700

    Fixed functions.py write_xyz definition

--- molecool/functions.py
+++ molecool/functions.py
@@ -68,3 +68,19 @@ def open_pdb(f_loc):
     coords = np.array(c)
     sym = np.arry(sym)
     return sym, coords
+
+def write_xyz(file_location, symbols, coordinates):
+    
+    ## Write an xyz file given a file location, symbols, and coordinates.
+    num_atoms = len(symbols)
+
+    if num_atom != len(coordinates):
+        raise ValueError(f"write_xyz : the number of symbols ({num_atoms}) and number of coordinates ({len(coordinates)}) must be the same to write syx file!")
+    
+    with open(file_location, 'w+') as f:
+        f.write('{}\n'.format(num_atoms))
+        f.write('XYZ file\n')
+        
+        for i in range(num_atoms):
+            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i], 
+                                              coordinates[i,0], coordinates[i,1], coordinates[i,2]))
